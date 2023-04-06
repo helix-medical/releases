b65cce9f32a3060efee71b96a90e02fee7d79132
Xavier2p <xavier.de-place@epita.fr>
2023-04-06 10:04:37 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/helix-medical/client
+
+git clone https://github.com/helix-medical/server
+
+cd client && npm install
+cd ../server && npm install
+
+echo 'Successfully installed, please start server by `npm run dev` and client by `npm start`
