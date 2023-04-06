fb2f3d4b02da87a314db998fa46d5a060e27cd35
Xavier2p <xavier.de-place@epita.fr>
2023-04-06 10:13:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,17 @@ git clone https://github.com/helix-medical/server
 cd client && npm install
 cd ../server && npm install
 
-echo 'Successfully installed, please start server by `npm run dev` and client by `npm start`
+cat << EOF >&2
+
+All is successfully installed!!
+To start Helix:
+1. Open a new terminal, go to this location and run:
+  > cd server && npm run dev
+
+2. In this terminal, run:
+  > cd client && npm start
+
+3. Go to http://localhost:3000/
+
+4. Enjoy!
+EOF
